d1303283f85b6d75452dc769e57c61c1b18a63a2
Felix Schäfer <f.schaefer@finn.de>
2011-09-18 19:11:18 +0200

    Add NewsController spec.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -12,36 +12,14 @@ FactoryGirl.define do
     status User::STATUS_ACTIVE
     admin false
   end
+
+  factory :user_admin, :class => User do
+    firstname 'Redmine'
+    lastname 'Admin'
+    login 'admin'
+    mail 'admin@example.com'
+    password 'admin'
+    password_confirmation 'admin'
+    admin true
+  end
 end
-
-
-#Factory.define :user do |u|
-#  u.firstname 'Bob'
-#  u.lastname 'Bobbit'
-#  u.sequence(:login) { |n| "bob#{n}" }
-#  u.sequence(:mail) {|n| "bob#{n}.bobbit@bob.com" }
-#  u.password 'admin'
-#  u.password_confirmation 'admin'
-#
-#  u.mail_notification(Redmine::VERSION::MAJOR > 0 ? 'all' : true)
-#
-#  u.language 'en'
-#  u.status User::STATUS_ACTIVE
-#  u.admin false
-#end
-#
-#Factory.define :admin, :class => User do |u|
-#  u.firstname 'Redmine'
-#  u.lastname 'Admin'
-#  u.login 'admin'
-#  u.password 'admin'
-#  u.password_confirmation 'admin'
-#  u.mail 'admin@example.com'
-#  u.admin true
-#end
-#
-#Factory.define :anonymous, :class => AnonymousUser do |u|
-#  u.lastname "Anonymous"
-#  u.firstname ""
-#  u.status User::STATUS_ANONYMOUS
-#end
