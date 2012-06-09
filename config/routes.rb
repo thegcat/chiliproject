140438f777e9e7693c50c269921d04d5c29aaaee
Felix Schäfer <felix@fachschaften.org>
2012-06-09 12:25:56 +0200

    Remove duplicate and old/deprecated route

--- config/routes.rb
+++ config/routes.rb
@@ -115,7 +115,7 @@ ActionController::Routing::Routes.draw do |map|
     issues.resources :time_entries, :controller => 'timelog'
   end
 
-  map.resources :issues, :path_prefix => '/projects/:project_id', :collection => { :create => :post } do |issues|
+  map.resources :issues, :path_prefix => '/projects/:project_id' do |issues|
     issues.resources :time_entries, :controller => 'timelog'
   end
 
